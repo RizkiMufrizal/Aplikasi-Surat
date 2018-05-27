b7000a1e74eddc400778c1021183f44e4c5c813d
Gogs <mufrizalrizki@gmail.com>
2018-05-27 22:56:16 +0700

    initial project

---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/AplikasiSuratApplication.java
@@ -0,0 +1,12 @@
+package org.rizki.mufrizal.aplikasi.surat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiSuratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiSuratApplication.class, args);
+	}
+}
