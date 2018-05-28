1e8ab2821f0f1592f929c07b5f8403f06a662499
rizkimufrizal <mufrizalrizki@gmail.com>
2018-05-29 05:22:22 +0700

    add roles data

---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/util/SetOfRoles.java
@@ -0,0 +1,23 @@
+package org.rizki.mufrizal.aplikasi.surat.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 29 May 2018
+ * @Time 4:27 AM
+ * @Project Aplikasi-Surat
+ * @Package org.rizki.mufrizal.aplikasi.surat.util
+ * @File SetOfRoles
+ */
+public class SetOfRoles {
+    public static Set<String> getRoles() {
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("ROLE_ADMIN");
+        stringSet.add("ROLE_PIMPINAN");
+        stringSet.add("KEPALA_BAGIAN");
+        return stringSet;
+    }
+}
