41e85e9ae7c50c56579f072f5ae0001562aae645
rizkimufrizal <mufrizalrizki@gmail.com>
2018-05-28 23:41:33 +0700

    add crud user

---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/service/UserService.java
@@ -0,0 +1,26 @@
+package org.rizki.mufrizal.aplikasi.surat.service;
+
+import org.rizki.mufrizal.aplikasi.surat.domain.User;
+
+import java.util.Optional;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 28 May 2018
+ * @Time 10:58 PM
+ * @Project Aplikasi-Surat
+ * @Package org.rizki.mufrizal.aplikasi.surat.service
+ * @File UserService
+ */
+public interface UserService {
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(String username);
+
+    Optional<User> findById(String username);
+
+    Iterable<User> findAll();
+}
---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/service/impl/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package org.rizki.mufrizal.aplikasi.surat.service.impl;
+
+import org.rizki.mufrizal.aplikasi.surat.domain.User;
+import org.rizki.mufrizal.aplikasi.surat.repository.UserRepository;
+import org.rizki.mufrizal.aplikasi.surat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 28 May 2018
+ * @Time 11:04 PM
+ * @Project Aplikasi-Surat
+ * @Package org.rizki.mufrizal.aplikasi.surat.service.impl
+ * @File UserServiceImpl
+ */
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(String username) {
+        userRepository.deleteById(username);
+    }
+
+    @Override
+    public Optional<User> findById(String username) {
+        return userRepository.findById(username);
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+}
