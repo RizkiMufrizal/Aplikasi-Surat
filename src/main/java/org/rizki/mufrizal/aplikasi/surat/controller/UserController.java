62136df329494edbd331f7cf5f39df10b8b2dc64
rizkimufrizal <mufrizalrizki@gmail.com>
2018-05-29 05:22:54 +0700

    add controller user for save

--- src/main/java/org/rizki/mufrizal/aplikasi/surat/controller/UserController.java
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/controller/UserController.java
@@ -1,10 +1,15 @@
 package org.rizki.mufrizal.aplikasi.surat.controller;
 
+import org.rizki.mufrizal.aplikasi.surat.domain.User;
 import org.rizki.mufrizal.aplikasi.surat.service.UserService;
+import org.rizki.mufrizal.aplikasi.surat.util.SetOfRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
@@ -25,6 +30,37 @@ public class UserController {
     public String userIndex(Model model) {
         model.addAttribute("users", userService.findAll());
         return "users";
+    }
+
+    @GetMapping(value = "/newUsers")
+    public String userNew(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", SetOfRoles.getRoles());
+        return "user_new";
+    }
+
+    @PostMapping(value = "/newUsers")
+    public String userSave(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/editUsers/{username}")
+    public String userEdit(@PathVariable("username") String username, Model model) {
+        model.addAttribute("user", userService.findById(username));
+        return "user_edit";
+    }
+
+    @PostMapping(value = "/editUsers")
+    public String userUpdate(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/deleteUsers/{username}")
+    public String userDelete(@PathVariable("username") String username) {
+        userService.deleteUser(username);
+        return "redirect:/users";
     }
 
 }
