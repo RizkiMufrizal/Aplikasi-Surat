295f32ce0a5d5fe58d3c8f7eed32398f347a28f6
rizkimufrizal <mufrizalrizki@gmail.com>
2018-05-29 16:03:59 +0700

    add custom bean for mapper

---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/dto/mapper/custom/StringToLocalDate.java
@@ -0,0 +1,27 @@
+package org.rizki.mufrizal.aplikasi.surat.dto.mapper.custom;
+
+import org.mapstruct.Named;
+import org.rizki.mufrizal.aplikasi.surat.helper.DateFormatterHelper;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 29 May 2018
+ * @Time 3:42 PM
+ * @Project Aplikasi-Surat
+ * @Package org.rizki.mufrizal.aplikasi.surat.dto.mapper.custom
+ * @File StringToLocalDate
+ */
+@Component
+@Named("StringToLocalDate")
+public class StringToLocalDate {
+
+    @Named("toLocalDateTime")
+    public LocalDateTime toLocalDateTime(String letterDate) {
+        return DateFormatterHelper.fromStringToLocalDateTime(letterDate, "yyyy-MM-dd HH:mm:ss");
+    }
+
+}
