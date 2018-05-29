0dc568c7ec40d45b9b6b130fbe6af7fbfef09e0d
rizkimufrizal <mufrizalrizki@gmail.com>
2018-05-29 16:03:25 +0700

    add repository

---
+++ src/main/java/org/rizki/mufrizal/aplikasi/surat/repository/LetterRepository.java
@@ -0,0 +1,19 @@
+package org.rizki.mufrizal.aplikasi.surat.repository;
+
+import org.rizki.mufrizal.aplikasi.surat.domain.Letter;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 29 May 2018
+ * @Time 12:59 PM
+ * @Project Aplikasi-Surat
+ * @Package org.rizki.mufrizal.aplikasi.surat.repository
+ * @File LetterRepository
+ */
+public interface LetterRepository extends PagingAndSortingRepository<Letter, String> {
+    Iterable<Letter> findAllByIsLetPassIsTrue();
+
+    Iterable<Letter> findAllByIsLetPassIsFalse();
+}
